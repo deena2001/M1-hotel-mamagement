294c9ad03d3f95be552ec3b9386817cd5fb36848
deena2001 <74053403+deena2001@users.noreply.github.com>
2022-02-10 18:03:03 +0530

    Add files via upload

---
+++ 3_Implementation/inc/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+
+void add();  //FUNCTIONS
+void list();
+void edit();  //GLOBALLY DECLARED FUNCTIONS N VARIABLE
+void delete1();
+void search();
+#endif // MAIN_H_INCLUDED
